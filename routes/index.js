e16ed50edb3b926a59941fc8347c6addb1697f91
Isabel Gonzalez <isabelggc@gmail.com>
2015-07-05 22:10:00 +0200

    Creacion de vista de créditos

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/', function(req, res) {
 router.get('/quizzes/question', quizController.question);
 router.get('/quizzes/answer', quizController.answer);
 
+router.get('/author', function(req, res) {
+	res.render('author');
+});
+
 module.exports = router;

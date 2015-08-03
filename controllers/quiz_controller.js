d6f0eac317f02d983e34b098f0de4fe147205a9e
Isabel Gonzalez <isabelggc@gmail.com>
2015-08-03 09:44:49 +0200

    Modelo y base de datos con sequelize y sqlite

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,21 @@
+var models = require('../models/models.js');
+
 /* GET /quizzes/question */
 exports.question = function(req, res) {
-  res.render('quizzes/question', { pregunta: '¿Cuál es la capital de Italia?' });
+  models.Quiz.findAll().success(function(quiz){
+    res.render('quizzes/question', { pregunta: quiz[0].pregunta });
+  });
 };
 
 
 /* GET /quizzes/answer */
 exports.answer = function(req, res) {
-  if (req.query.respuesta === 'Roma') {
-    res.render('quizzes/answer', { respuesta: 'Correcto' });
-  } else {
-    res.render('quizzes/answer', { respuesta: 'Incorrecto' });
-  }
+  models.Quiz.findAll().success(function(quiz){
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizzes/answer', { respuesta: 'Correcto' });
+    } else {
+      res.render('quizzes/answer', { respuesta: 'Incorrecto' });
+    }
+  });
 };
 

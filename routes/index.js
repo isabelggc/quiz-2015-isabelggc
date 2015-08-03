ff302753a200024d08fa11f323759f3171551b52
Isabel Gonzalez <isabelggc@gmail.com>
2015-08-03 11:17:00 +0200

    Manejo de varias preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizzes/question', quizController.question);
-router.get('/quizzes/answer', quizController.answer);
+// Definición de rutas de /quizzes
+router.get('/quizzes', quizController.index);
+router.get('/quizzes/:quizId(\\d+)', quizController.show);
+router.get('/quizzes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', function(req, res) {
 	res.render('author');

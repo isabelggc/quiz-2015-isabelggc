f91432709425aa343439515de0f93a19a27f498e
Isabel Gonzalez <isabelggc@gmail.com>
2015-08-03 15:45:34 +0200

    Borrar npm modules del commit

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,8 @@ router.get('/quizzes', quizController.index);
 router.get('/quizzes/:quizId(\\d+)', quizController.show);
 router.get('/quizzes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/author', function(req, res) {
-	res.render('author');
+	res.render('author', { errors:[]});
+
 });
 router.get('/quizzes/new', quizController.new);
 router.post('/quizzes/create', quizController.create);

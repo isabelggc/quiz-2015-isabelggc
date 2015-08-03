201037a1d3adb56496f8165d06d98b1a68827926
Isabel Gonzalez <isabelggc@gmail.com>
2015-08-04 00:04:50 +0200

    Autenticación y sesión

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var partials = require('express-partials');
 
 var routes = require('./routes/index');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 var app = express();
 // view engine setup
@@ -22,9 +23,22 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Helpers dinámicos
+app.use(function(req, res, next){
+  // guardar path en session.redir para después de login
+  if (!req.path.match(/\/login|\/logout/)) {
+    req.session.redir = req.path;
+  }
+
+  // Hacer visible req.session en las vistas
+  res.locals.session = req.session;
+  next();
+});
 
 app.use('/', routes);
 
